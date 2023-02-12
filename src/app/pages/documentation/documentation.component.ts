d83f26968bf236679a061a13f86cd9e7b6fb6ddc
omolinsc <oriol.molins@bootcamp-upgrade.com>
2023-02-12 12:16:47 +0100

    FEAT: Documentation working with HTML, CSS and JS, delete butons, and testing insert

--- src/app/pages/documentation/documentation.component.ts
+++ src/app/pages/documentation/documentation.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { ApiService } from './../../shared/services/api.service';
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -11,16 +13,35 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class DocumentationComponent implements OnInit{
 
   apiInfo! : any;
-  url: string = "http://localhost:3000/html";
+  sections: string[] = ["html", "css", "js"];
+  html! :any;
+  showHtml: boolean = false;
+  css!: any;
+  showCss: boolean = false;
+  js!: any;
+  showJs: boolean = false;
+  showDelete: boolean = false;
+  baseUrl: string = `http://localhost:3000/`;
+  
 
-  constructor(private api:ApiService, private _sanitizer: DomSanitizer){ }
+  constructor(private api:ApiService, private _sanitizer: DomSanitizer, private router: Router){ }
 
   ngOnInit(): void {
-      this.api.getInfo(this.url).subscribe((data:any) => {
-        this.apiInfo = data;
-        console.log(this.apiInfo);
+
+      this.api.getInfo(this.baseUrl+"html").subscribe((data:any) => {
+        this.html = data;
+        console.log(this.html);
       })
 
+      this.api.getInfo(this.baseUrl+"css").subscribe((data:any) => {
+        this.css = data;
+        console.log(this.css);
+      })
+
+      this.api.getInfo(this.baseUrl+"js").subscribe((data:any) => {
+        this.js = data;
+        console.log(this.js);
+      })
   }
 
   getVideoIframe(url:string) {
@@ -33,5 +54,34 @@ export class DocumentationComponent implements OnInit{
     video   = (results === null) ? url : results[1];
  
     return this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + video);   
+  }
+
+  deleteArticle(id:number){
+    console.log(id)
+    this.api.deleteFormulary(id).subscribe((data) => {
+      this.ngOnInit();
+      this.router.navigate(["/documentation"])
+    })
+  }
+
+  switchHtml(){
+    this.showHtml = !this.showHtml
+    this.showCss = false
+    this.showJs = false
+  }
+
+  switchCss(){
+    this.showHtml = false
+    this.showCss = !this.showCss
+    this.showJs = false 
+  }
+
+  switchJs(){
+    this.showHtml = false
+    this.showCss = false
+    this.showJs = !this.showJs
+  }
+  switchDelete(){
+    this.showDelete = !this.showDelete
   }
 }
--- src/app/shared/interfaces/istructure.ts
+++ src/app/shared/interfaces/istructure.ts
@@ -1,9 +1,10 @@
 export interface IStructure {
         id: number;
-        img: string;
+        img?: string;
         title: string;
         author: string;
         description: string;
-        url: string;
+        url?: string;
+        video?: string;
         tag: string;
 }
